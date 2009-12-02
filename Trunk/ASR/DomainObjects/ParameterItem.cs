eb9c3126ece7455ecd1796f5d96b51f26d89a020
RaulJimenez <RaulJimenez@40e8ca83-592f-4f66-97f7-3210ca4fa8d7>
2009-12-02 16:59:20 +0000

    git-svn-id: http://svn.sitecore.net/AdvancedSystemReporter@18 40e8ca83-592f-4f66-97f7-3210ca4fa8d7

--- Trunk/ASR/DomainObjects/ParameterItem.cs
+++ Trunk/ASR/DomainObjects/ParameterItem.cs
@@ -2,6 +2,8 @@
 using CorePoint.DomainObjects.SC;
 using System.Collections.Generic;
 using System;
+using System.Collections.Specialized;
+using Sitecore.Web.UI.HtmlControls;
 
 namespace ASR.DomainObjects
 {
@@ -48,16 +50,93 @@ namespace ASR.DomainObjects
 			}
 		}
 
+        [Field("parameters")]
+        public string _parameters { get; set; }
+
+        private NameValueCollection _params;
+        public NameValueCollection Parameters
+        {
+            get
+            {
+                if (_params == null)
+                {
+                    //_params = Sitecore.StringUtil.ParseNameValueCollection(_parameters, '|', '=');
+                    _params = new NameValueCollection();
+                    string[] substrings = _parameters.Split('|');
+                    foreach (var st in substrings)
+                    {
+                        int i = st.IndexOf('=');
+                        if (i > 0)
+                        {
+                            _params.Add(st.Substring(0, i), st.Substring(i+1));
+                        }
+                    }
+                }
+                return _params;
+            }
+        }
+
 		public IEnumerable<ValueItem> PossibleValues()
 		{
 			return this.Director.GetChildObjects<ValueItem>(this.Id);
 		}
 
-		//public string Value
-		//{
-		//    get;
-		//    set;
-		//}
+        public Control MakeControl()
+        {
+            Control input = null;
+            if (this.Type == "Text")
+            {
+                input = new Edit();
+                input.ID = Control.GetUniqueID("input");
+            }
+            else if (this.Type == "Dropdown")
+            {
+                Combobox c = new Combobox();
+                foreach (var value in this.PossibleValues())
+                {
+                    ListItem li = new ListItem();
+                    li.Header = value.Name;
+                    li.Value = value.Value;
+                    c.Controls.Add(li);
+                }
+                input = c;
+                input.ID = Control.GetUniqueID("input");
+            }
+            else if (this.Type == "Item Selector")
+            {
+                ASR.Controls.ItemSelector iSelect = new ASR.Controls.ItemSelector();
+                input = iSelect;
+                input.ID = Control.GetUniqueID("input");
+                iSelect.Click = string.Concat("itemselector", ":", input.ID);
+                if (this.Parameters["root"] != null) iSelect.Root = this.Parameters["root"];
+                if (this.Parameters["folder"] != null) iSelect.Folder = this.Parameters["folder"];
+                if (this.Parameters["displayresult"] != null) iSelect.DisplayValueType = (ASR.Controls.ItemInfo)Enum.Parse(typeof(ASR.Controls.ItemInfo), this.Parameters["displayresult"].ToString());
+                if (this.Parameters["valueresult"] != null) iSelect.ValueType = (ASR.Controls.ItemInfo)Enum.Parse(typeof(ASR.Controls.ItemInfo), this.Parameters["valueresult"].ToString());
+                if (this.Parameters["filter"] != null) iSelect.Filter = this.Parameters["filter"];
+            }
+            else if (this.Type == "User Selector")
+            {
+                ASR.Controls.UserSelector uSelect = new ASR.Controls.UserSelector();
+                input = uSelect;
+                input.ID = Control.GetUniqueID("input");
+                uSelect.Click = string.Concat("itemselector", ":", input.ID);
+                if (this.Parameters["filter"] != null) uSelect.Filter = this.Parameters["filter"];
+            }
+            else if (this.Type == "Date picker")
+            {
+                DateTimePicker dtPicker = new ASR.Controls.DateTimePicker();
+                dtPicker.Style.Add("float", "left");
+                dtPicker.ID = Sitecore.Web.UI.HtmlControls.Control.GetUniqueID("input");
+                dtPicker.ShowTime = false;
+                dtPicker.Click = "datepicker" + ":" + dtPicker.ID;
+                dtPicker.Style.Add(System.Web.UI.HtmlTextWriterStyle.Display, "inline");
+                dtPicker.Style.Add(System.Web.UI.HtmlTextWriterStyle.VerticalAlign, "middle");
+                input = dtPicker;
+            }
+
+            input.Value = this.Value;
+            return input;
+        }
 
 	}
 }
