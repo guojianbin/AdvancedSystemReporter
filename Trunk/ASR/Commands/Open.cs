73c2f99891003c8fdebb265b4543c03326da27e2
RaulJimenez <RaulJimenez@40e8ca83-592f-4f66-97f7-3210ca4fa8d7>
2009-12-03 16:16:28 +0000

    git-svn-id: http://svn.sitecore.net/AdvancedSystemReporter@28 40e8ca83-592f-4f66-97f7-3210ca4fa8d7

--- Trunk/ASR/Commands/Open.cs
+++ Trunk/ASR/Commands/Open.cs
@@ -2,6 +2,8 @@
 using Sitecore.Web.UI.Sheer;
 using CorePoint.DomainObjects.SC;
 using ASR.DomainObjects;
+using Sitecore.Data.Items;
+using Sitecore.Data;
 
 namespace ASR.Commands
 {
@@ -33,15 +35,41 @@ namespace ASR.Commands
                 {
                     return;
                 }
-                SCDirector director = new SCDirector(Current.Context.Settings.ConfigurationDatabase, "en");
-                ReportItem rItem = director.GetObjectByIdentifier<ReportItem>(args.Result);
-                if (rItem != null)
+                Database database = Sitecore.Configuration.Factory.GetDatabase(Current.Context.Settings.ConfigurationDatabase);
+                Sitecore.Diagnostics.Assert.IsNotNull(database,"no configuration databsae");
+
+                Item item = database.GetItem(args.Result);
+
+                Sitecore.Diagnostics.Assert.IsNotNull(item, "report item cannot be loaded");
+
+                switch(item.Template.Key)
                 {
-                    Current.Context.ReportItem = rItem;
-                    Current.Context.Report = null;
-                    Sitecore.Context.ClientPage.SendMessage(this, "ASR.MainForm:update");
-                   // Sitecore.Context.ClientPage.SendMessage(this, "ASR.MainForm:toolbarupdate");
+                    case "report":
+                    SCDirector director = new SCDirector(Current.Context.Settings.ConfigurationDatabase, "en");
+
+                    ReportItem rItem = director.LoadObjectFromItem<ReportItem>(item);
+                    if (rItem != null)
+                    {
+                        Current.Context.ReportItem = rItem;
+                        Current.Context.Report = null;
+                        Sitecore.Context.ClientPage.SendMessage(this, "ASR.MainForm:update");
+                  
+                        // Sitecore.Context.ClientPage.SendMessage(this, "ASR.MainForm:toolbarupdate");
+                    }
+                        break;
+                   
+                    case "saved report":
+                
+                    Message m = Message.Parse(this, "ASR.MainForm:openlink");
+                    System.Collections.Specialized.NameValueCollection nvc = 
+                    Sitecore.StringUtil.ParseNameValueCollection(item["parameters"], '|', '=');
+
+                    m.Arguments.Add(nvc);
+                    Sitecore.Context.ClientPage.SendMessage(m);
+                    break;
                 }
+
+               
             }
         }
 
