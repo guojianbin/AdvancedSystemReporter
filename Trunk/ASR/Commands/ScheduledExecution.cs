197e3451d05302d151a4fa5f67d273b48c848178
RaulJimenez <RaulJimenez@40e8ca83-592f-4f66-97f7-3210ca4fa8d7>
2009-12-03 16:17:03 +0000

    git-svn-id: http://svn.sitecore.net/AdvancedSystemReporter@32 40e8ca83-592f-4f66-97f7-3210ca4fa8d7

---
+++ Trunk/ASR/Commands/ScheduledExecution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Tasks;
+using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
+
+namespace ASR.Commands
+{
+    public class ScheduledExecution
+    {
+        public void EmailReports(Item[] itemarray, CommandItem commandItem, ScheduleItem scheduleItem)
+        {
+            //MultilistField mf = commandItem.InnerItem.Fields["reports"];
+            //if (mf != null)
+            //{
+            //    foreach (Item item in mf.GetItems())
+            //    {
+            //        runReport(item);
+            //    }
+            //}
+        }
+
+        private void runReport(Item item)
+        {
+            throw new NotImplementedException(item.Name);
+        }
+    }
+}
