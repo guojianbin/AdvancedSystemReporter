7e334c4a5864926c33b54157d601e3475086a26c
RaulJimenez <RaulJimenez@40e8ca83-592f-4f66-97f7-3210ca4fa8d7>
2010-04-23 15:11:50 +0000

    FIXED: link and general link fields could throw a null reference exception
    git-svn-id: http://svn.sitecore.net/AdvancedSystemReporter@46 40e8ca83-592f-4f66-97f7-3210ca4fa8d7

--- Trunk/Reports/Viewers/ItemViewer.cs
+++ Trunk/Reports/Viewers/ItemViewer.cs
@@ -3,213 +3,230 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ASR.Interface;
-using ASR.Reports.Items.Exceptions;
 using Sitecore;
-using Sitecore.Collections;
+using Sitecore.Data;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
 using Sitecore.Workflows;
 
 namespace ASR.Reports.Items
 {
-	public class ItemViewer : ASR.Interface.BaseViewer
-	{
-		public static string COLUMNS_PARAMETER = "columns";
-		public static string HEADERS_PARAMETER = "headers";
-        public static string MAX_LENGHT_PARAMETER = "maxlength";
+  public class ItemViewer : BaseViewer
+  {
+    #region Fields
 
-        private int _maxLength = -1;
-        public int MaxLength
+    public static string COLUMNS_PARAMETER = "columns";
+    public static string HEADERS_PARAMETER = "headers";
+    public static string MAX_LENGHT_PARAMETER = "maxlength";
+
+    private int maxLength = -1;
+
+    #endregion
+
+    #region Public properties
+
+    public int MaxLength
+    {
+      get
+      {
+        if (maxLength < 0)
         {
-            get
-            {
-                if (_maxLength < 0)
-                {
-                    if (!int.TryParse(base.getParameter(MAX_LENGHT_PARAMETER), out _maxLength))
-                    {
-                        _maxLength = 100;
-                    }
-                }
-                return _maxLength;
-            }
+          if (!int.TryParse(getParameter(MAX_LENGHT_PARAMETER), out maxLength))
+          {
+            maxLength = 100;
+          }
         }
-		public override void Display(DisplayElement d_element)
-		{
-			Item itemElement = d_element.Element as Item;
-			if (itemElement == null)
-			{
-				return;
-			}
-			d_element.Value = itemElement.Uri.ToString();
+        return maxLength;
+      }
+    }
 
-			d_element.Header = itemElement.Name;
+    #endregion
 
-			for (int i = 0; i < Columns.Count; i++)
-			{
-				ASR.DomainObjects.Column column = Columns[i];
+    #region Public methods
 
-				string text = getColumnText(column.Name, itemElement);
+    public override void Display(DisplayElement d_element)
+    {
+      var itemElement = d_element.Element as Item;
+      if (itemElement == null)
+      {
+        return;
+      }
+      d_element.Value = itemElement.Uri.ToString();
 
-				if (string.IsNullOrEmpty(text))
-				{
-					d_element.AddColumn(column.Header, itemElement[column.Name]);
-				}
-				else
-				{
-					d_element.AddColumn(column.Header, text);
-				}
-			}
-			d_element.Icon = itemElement.Appearance.Icon;
-		}
+      d_element.Header = itemElement.Name;
 
-		protected virtual string formatDateField(Item item, Sitecore.Data.ID fieldID)
-		{
-			DateField field = item.Fields[fieldID];
-			if (field != null && !String.IsNullOrEmpty(field.Value))
-			{
-				return field.DateTime.ToString("dd/MM/yyyy HH:mm");
-			}
-			return string.Empty;
-		}
+      foreach (var column in Columns)
+      {
+        var text = getColumnText(column.Name, itemElement);
 
-		protected virtual string getColumnText(string name, Item itemElement)
-		{
-			switch (name)
-			{
-				case "name":
-					return itemElement.Name;
+        d_element.AddColumn(column.Header, string.IsNullOrEmpty(text) ? itemElement[column.Name] : text);
+      }
+      d_element.Icon = itemElement.Appearance.Icon;
+    }
 
-				case "display name":
-					return itemElement.DisplayName;
+    #endregion
 
-				case "created by":
-					return itemElement[FieldIDs.CreatedBy];
+    #region Protected methods
 
-				case "updated":
-					return formatDateField(itemElement, FieldIDs.Updated);
+    protected virtual string formatDateField(Item item, ID fieldID)
+    {
+      DateField field = item.Fields[fieldID];
+      if (field != null && !String.IsNullOrEmpty(field.Value))
+      {
+        return field.DateTime.ToString("dd/MM/yyyy HH:mm");
+      }
+      return string.Empty;
+    }
 
-				case "updated by":
-					return itemElement[FieldIDs.UpdatedBy];
+    protected virtual string getColumnText(string name, Item itemElement)
+    {
+      switch (name)
+      {
+        case "name":
+          return itemElement.Name;
 
-				case "created":
-					return formatDateField(itemElement, FieldIDs.Created);
+        case "display name":
+          return itemElement.DisplayName;
 
-				case "locked by":
-					Sitecore.Data.Fields.LockField lf = itemElement.Fields["__lock"];
-					string locked = "unlocked";
-					if (lf != null)
-					{
-						if (!string.IsNullOrEmpty(lf.Owner))
-							locked = lf.Owner + " " + lf.Date.ToString("dd/MM/yy HH:mm");
-					}
-					return locked;
-				case "template":
-					return itemElement.Template.Name;
+        case "created by":
+          return itemElement[FieldIDs.CreatedBy];
 
-				case "path":
-					return itemElement.Paths.FullPath;
+        case "updated":
+          return formatDateField(itemElement, FieldIDs.Updated);
 
-				case "owner":
-					return itemElement[FieldIDs.Owner];
+        case "updated by":
+          return itemElement[FieldIDs.UpdatedBy];
 
-				case "workflow":
-					return getWorkflowInfo(itemElement);
+        case "created":
+          return formatDateField(itemElement, FieldIDs.Created);
 
-				case "children count":
-					return itemElement.Children.Count.ToString();
+        case "locked by":
+          LockField lf = itemElement.Fields["__lock"];
+          var text = "unlocked";
+          if (lf != null)
+          {
+            if (!string.IsNullOrEmpty(lf.Owner))
+              text = lf.Owner + " " + lf.Date.ToString("dd/MM/yy HH:mm");
+          }
+          return text;
+        case "template":
+          return itemElement.Template.Name;
 
-				case "version":
-					return itemElement.Version.ToString();
+        case "path":
+          return itemElement.Paths.FullPath;
 
-				case "language":
-					return itemElement.Language.CultureInfo.DisplayName;
-				default:
-					return getFriendlyFieldValue(name, itemElement);
+        case "owner":
+          return itemElement[FieldIDs.Owner];
 
-			}
-		}
+        case "workflow":
+          return getWorkflowInfo(itemElement);
 
-		private string getFriendlyFieldValue(string name, Item itemElement)
-		{
-			Field field = itemElement.Fields[name];
-			if (field != null)
-			{
-				switch (field.TypeKey)
-				{
-					case "date":
-					case "datetime":
-						return formatDateField(itemElement, field.ID);
-					case "droplink":
-					case "droptree":
-                    case "reference":
-                    case "grouped droplink":
-						LookupField lookupFld = (LookupField)field;
-						if (lookupFld != null && lookupFld.TargetItem != null)
-						{
-							return lookupFld.TargetItem.Name;
-						}
-						break;                    
-					case "checklist":
-					case "multilist":
-					case "treelist":
-					case "treelistex":
-						MultilistField multilistField = (MultilistField)field;
-						if (multilistField != null)
-						{
-							StringBuilder strBuilder = new StringBuilder();
-							foreach (Item item in multilistField.GetItems())
-							{
-								strBuilder.AppendFormat("{0}, ", item.Name);
-							}
-							return StringUtil.Clip(strBuilder.ToString().TrimEnd(',', ' '),MaxLength,true);
-						}
-						break;
-                    case "link":
-                    case "general link":
-                        LinkField lf = new LinkField(field);
-                        switch (lf.LinkType)
-                        {
-                            case "media":
-                            case "internal": return lf.TargetItem.Paths.ContentPath;
-                            case "anchor":
-                            case "mailto":
-                            case "external": return lf.Url;
-                            default:
-                                return lf.Text;
-                        }
-					default:
-						return StringUtil.Clip(field.Value,MaxLength,true);
-				}
-			}
-			return String.Empty;
-		}
+        case "children count":
+          return itemElement.Children.Count.ToString();
 
-		private string getWorkflowInfo(Item itemElement)
-		{
-			System.Text.StringBuilder sb = new System.Text.StringBuilder();
-			Sitecore.Workflows.IWorkflow iw = itemElement.State.GetWorkflow();
-			if (iw != null)
-			{
-				sb.Append(iw.Appearance.DisplayName);
-			}
-			Sitecore.Workflows.WorkflowState ws = itemElement.State.GetWorkflowState();
+        case "version":
+          return itemElement.Version.ToString();
 
-			if (ws != null)
-			{
-				sb.AppendFormat(" ({0})", ws.DisplayName);
-			}
+        case "language":
+          return itemElement.Language.CultureInfo.DisplayName;
+        default:
+          return getFriendlyFieldValue(name, itemElement);
+      }
+    }
 
-			if (iw != null)
-			{
-				IEnumerable<WorkflowEvent> events = iw.GetHistory(itemElement).OrderByDescending(e => e.Date);
-				IEnumerator<WorkflowEvent> enumerator = events.GetEnumerator();
-				if (enumerator.MoveNext())
-				{
-					TimeSpan span = DateTime.Now.Subtract(enumerator.Current.Date);
-					sb.AppendFormat(" for {0} days {1} hours {2} minutes", span.Days, span.Hours, span.Minutes);
-				}
-			}
-			return sb.ToString();
-		}
-	}
+    #endregion
+
+    #region Private methods
+
+    private static string getWorkflowInfo(Item itemElement)
+    {
+      var sb = new StringBuilder();
+      var iw = itemElement.State.GetWorkflow();
+      if (iw != null)
+      {
+        sb.Append(iw.Appearance.DisplayName);
+      }
+      var ws = itemElement.State.GetWorkflowState();
+
+      if (ws != null)
+      {
+        sb.AppendFormat(" ({0})", ws.DisplayName);
+      }
+
+      if (iw != null)
+      {
+        IEnumerable<WorkflowEvent> events = iw.GetHistory(itemElement).OrderByDescending(e => e.Date);
+        var enumerator = events.GetEnumerator();
+        if (enumerator.MoveNext())
+        {
+          var span = DateTime.Now.Subtract(enumerator.Current.Date);
+          sb.AppendFormat(" for {0} days {1} hours {2} minutes", span.Days, span.Hours, span.Minutes);
+        }
+      }
+      return sb.ToString();
+    }
+
+    private string getFriendlyFieldValue(string name, Item itemElement)
+    {
+      var field = itemElement.Fields[name];
+      if (field != null)
+      {
+        switch (field.TypeKey)
+        {
+          case "date":
+          case "datetime":
+            return formatDateField(itemElement, field.ID);
+          case "droplink":
+          case "droptree":
+          case "reference":
+          case "grouped droplink":
+            var lookupFld = (LookupField) field;
+            if (lookupFld != null && lookupFld.TargetItem != null)
+            {
+              return lookupFld.TargetItem.Name;
+            }
+            break;
+          case "checklist":
+          case "multilist":
+          case "treelist":
+          case "treelistex":
+            var multilistField = (MultilistField) field;
+            if (multilistField != null)
+            {
+              var strBuilder = new StringBuilder();
+              foreach (var item in multilistField.GetItems())
+              {
+                strBuilder.AppendFormat("{0}, ", item.Name);
+              }
+              return StringUtil.Clip(strBuilder.ToString().TrimEnd(',', ' '), MaxLength, true);
+            }
+            break;
+          case "link":
+          case "general link":
+            var lf = new LinkField(field);
+            switch (lf.LinkType)
+            {
+              case "media":
+              case "internal":
+                if (lf.TargetItem != null)
+                {
+                  return lf.TargetItem.Paths.ContentPath;
+                }
+                return  lf.Value == string.Empty ? "[undefined]" : "[broken link] "+lf.Value;
+              case "anchor":
+              case "mailto":
+              case "external":
+                return lf.Url;
+              default:
+                return lf.Text;
+            }
+          default:
+            return StringUtil.Clip(field.Value, MaxLength, true);
+        }
+      }
+      return String.Empty;
+    }
+
+    #endregion
+  }
 }
