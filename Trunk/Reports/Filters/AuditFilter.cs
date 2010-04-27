020bf7ab85d96d700eb3707bd71c9ac53fda15c9
RaulJimenez <RaulJimenez@40e8ca83-592f-4f66-97f7-3210ca4fa8d7>
2010-04-27 12:26:57 +0000

    the verbs are now using contains instead of equals, it makes it more flexible and the parameters were designed this way
    git-svn-id: http://svn.sitecore.net/AdvancedSystemReporter@50 40e8ca83-592f-4f66-97f7-3210ca4fa8d7

--- Trunk/Reports/Filters/AuditFilter.cs
+++ Trunk/Reports/Filters/AuditFilter.cs
@@ -1,28 +1,27 @@
 ﻿
 namespace ASR.Reports.Logs
 {
-    public class AuditFilter : ASR.Interface.BaseFilter
+  public class AuditFilter : Interface.BaseFilter
+  {
+    public static string USER_PARAMETER = "user";
+    public static string VERB_PARAMETER = "verb";
+
+    public string Verb { get { return getParameter(VERB_PARAMETER); } }
+    public string User { get { return getParameter(USER_PARAMETER); } }
+
+    public override bool Filter(object element)
     {
-        public static string USER_PARAMETER = "user";
-        public static string VERB_PARAMETER = "verb";
+      var ai = element as AuditItem;
 
-        public string Verb { get { return getParameter(VERB_PARAMETER); } }
-        public string User { get { return getParameter(USER_PARAMETER); } }
+      if (ai == null)
+      {
+        return true;
+      }
 
-        public override bool Filter(object element)
-        {
-            AuditItem ai = element as AuditItem;
-
-            if (ai == null)
-            {
-                return true;
-            }
-            
-            return (string.IsNullOrEmpty(Verb) || ai.Verb == null ||
-                ai.Verb.Equals(Verb,System.StringComparison.InvariantCultureIgnoreCase)) 
-                && (string.IsNullOrEmpty(User) || ai.User == null ||
-                ai.User.Equals(User,System.StringComparison.InvariantCultureIgnoreCase));
-
-        }
+      return (string.IsNullOrEmpty(Verb) || ai.Verb == null ||
+              ai.Verb.Contains(Verb))
+              && (string.IsNullOrEmpty(User) || ai.User == null ||
+            ai.User.Equals(User, System.StringComparison.InvariantCultureIgnoreCase));
     }
+  }
 }
