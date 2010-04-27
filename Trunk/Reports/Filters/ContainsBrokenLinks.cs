6e1de493579860d3c18a376476b0b2b85909e780
RaulJimenez <RaulJimenez@40e8ca83-592f-4f66-97f7-3210ca4fa8d7>
2010-04-27 11:21:49 +0000

    git-svn-id: http://svn.sitecore.net/AdvancedSystemReporter@47 40e8ca83-592f-4f66-97f7-3210ca4fa8d7

---
+++ Trunk/Reports/Filters/ContainsBrokenLinks.cs
@@ -0,0 +1,18 @@
+﻿using ASR.Interface;
+using Sitecore.Data.Items;
+
+namespace ASR.Reports.Filters
+{
+  public class ContainsBrokenLinks : BaseFilter
+  {
+    public static string ALLVERSIONS_PARAMETER = "allversions";
+    public override bool Filter(object element)
+    {
+      var item = element as Item;
+      if (item == null) return false;
+      var allversions = "true" == getParameter(ALLVERSIONS_PARAMETER);
+      var brokenlinks = item.Links.GetBrokenLinks(allversions);
+      return (brokenlinks != null && brokenlinks.Length > 0);
+    }
+  }
+}
