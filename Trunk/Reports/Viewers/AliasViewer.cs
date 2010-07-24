5520c2eaafc2b399c0b8b20899c83bb8d7422c47
RaulJimenez <RaulJimenez@40e8ca83-592f-4f66-97f7-3210ca4fa8d7>
2010-07-24 22:05:42 +0000

    Improved performance by using a dictionary
    git-svn-id: http://svn.sitecore.net/AdvancedSystemReporter@70 40e8ca83-592f-4f66-97f7-3210ca4fa8d7

--- Trunk/Reports/Viewers/AliasViewer.cs
+++ Trunk/Reports/Viewers/AliasViewer.cs
@@ -1,73 +1,52 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 using ASR.Interface;
+using Sitecore.Data;
 using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Data.Fields;
 
 namespace ASR.Reports.Viewers
 {
     public class AliasViewer : BaseViewer
     {
-        private static string AliasTemplateID;
-
-        static AliasViewer()
-        {
-            AliasTemplateID = "{54BCFFB7-8F46-4948-AE74-DA5B6B5AFA86}";
-        }
-
-        private const string Usefastparameter = "usefast";
-
-        protected bool UseFastSearch
-        {
-            get
-            {
-                return getParameter(Usefastparameter) == "true";
-            }
-        }
+       
         public override void Display(DisplayElement dElement)
         {
             var item = dElement.Element as Item;
-            if (item != null)
-            {
-                //find if alias
-                var alias = FindAlias(item);
-                //add it to the report
+            if (item == null) return;
 
-                if(alias != null)
+            var alias = GetAlias(item);
+            if(alias != null) dElement.AddColumn("Alias",alias);
+        }
+
+        private Dictionary<ID, string> _alias;
+        private string GetAlias(Item item)
+        {
+            if (_alias == null)
+            {
+                InitializeAlias(item.Database);
+                Error.AssertNotNull(_alias,"error initializing alias table");
+            }
+            return _alias.ContainsKey(item.ID) ? _alias[item.ID] : null;
+        }
+
+        private void InitializeAlias(Database db)
+        {
+            _alias = new Dictionary<ID, string>();
+            var aliasparent = db.GetItem("/sitecore/system/aliases");
+            Error.AssertNotNull(aliasparent,"can't find aliases");
+            foreach (Item child in aliasparent.Children)
+            {
+                LinkField lf = child.Fields["linked item"];
+                if (_alias.ContainsKey(lf.TargetID))
                 {
-                    dElement.AddColumn("Alias",alias.Name);
+                    _alias[lf.TargetID] += string.Concat(", ", child.Name);
                 }
-            }          
-        }
-
-        private static Item FindAliasFast(Item item)
-        {
-            const string queryFormatString = "fast:/sitecore/system/aliases/*[@linked item ='%{0}%']";
-            var query = string.Format(queryFormatString, item.ID);
-            var items = item.Database.SelectItems(query);
-            if (items != null && items.Length > 0) return items[0];
-            return null;
-        }
-
-        private Item FindAlias(Item item)
-        {
-            return UseFastSearch ? FindAliasFast(item) : FindAliasLinks(item);
-        }
-
-        private static Item FindAliasLinks(Item item)
-        {
-            var linksDb = Sitecore.Configuration.Factory.GetLinkDatabase();
-            var itemlinks = linksDb.GetReferrers(item);
-            foreach (var itemLink in itemlinks)
-            {
-                if ( itemLink.SourceDatabaseName == item.Database.Name)
+                else
                 {
-                    var sourceitem = itemLink.GetSourceItem();
-                    if ( sourceitem.TemplateID.ToString() == AliasTemplateID)
-                    {
-                        return sourceitem;
-                    }
+                    _alias.Add(lf.TargetID, child.Name);                    
                 }
             }
-            return null;
         }
     }
 }
