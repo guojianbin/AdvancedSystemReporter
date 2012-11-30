c6b033fd63261344db3e45dfd554f23437ef755d
ChristianHagelid <ChristianHagelid@40e8ca83-592f-4f66-97f7-3210ca4fa8d7>
2012-11-30 03:21:24 +0000

    - branched into its new branch
    git-svn-id: http://svn.sitecore.net/AdvancedSystemReporter@93 40e8ca83-592f-4f66-97f7-3210ca4fa8d7

---
+++ Branches/EmailReportUpdates/ASR/Commands/ScheduledExecution.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+
+using Sitecore.Tasks;
+using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
+using Sitecore.Diagnostics;
+using ASR.DomainObjects;
+
+namespace ASR.Commands
+{
+    using System.Net.Mail;
+
+    using CommandItem = Sitecore.Tasks.CommandItem;
+    using Sitecore;
+
+    public class ScheduledExecution
+    {
+        private const string LogPrefix = "ASR.Email -- ";
+
+        public void EmailReports(Item[] itemarray, CommandItem commandItem, ScheduleItem scheduleItem)
+        {
+            var item = commandItem.InnerItem;
+            if (item["active"] != "1") return;
+
+            Log("Starting report email task");
+            MultilistField mf = item.Fields["reports"];
+            if (mf == null) return;
+            var force = item["sendempty"] == "1";
+            var filePaths = mf.GetItems().Select(i => runReport(i, force));
+
+
+            MailMessage mailMessage;
+
+            try
+            {
+                mailMessage = new MailMessage
+                {
+                    From = new MailAddress(item["from"]),
+                    Subject = item["subject"],
+                };
+            }
+            catch (Exception ex)
+            {
+                LogException("FROM email address error.");
+                return;
+            }
+
+            var senders = item["to"].Split(',');
+            foreach (var sender in senders)
+            {
+                // test that each email address is valid. Continue to the next if it isn't.
+                try
+                {
+                    var toAddress = new MailAddress(sender);
+                    mailMessage.To.Add(toAddress);
+                }
+                catch (Exception)
+                {
+                    LogException("TO email address error. " + sender);
+                }
+            }
+
+            mailMessage.Body = Sitecore.Web.UI.WebControls.FieldRenderer.Render(item, "text");
+            mailMessage.IsBodyHtml = true;
+
+            foreach (var path in filePaths.Where(st => !string.IsNullOrEmpty(st)))
+            {
+                mailMessage.Attachments.Add(new Attachment(path));
+            }
+
+            Log("attempting to send message");
+            MainUtil.SendMail(mailMessage);
+            Log("task report email finished");
+        }
+
+        private void Log(string message)
+        {
+            Sitecore.Diagnostics.Log.Info(string.Concat(LogPrefix, message), this);
+        }
+
+        private void LogException(string message)
+        {
+            Sitecore.Diagnostics.Log.Error(string.Concat(LogPrefix, message), this);
+        }
+        
+        private string runReport(Item item, bool force)
+        {
+            Assert.IsNotNull(item, "item");            
+            var reportItem = ReportItem.CreateFromParameters(item["parameters"]);
+            var prefix = reportItem.Name;
+            var report = reportItem.TransformToReport(null);
+            report.Run(null);
+            Log(string.Concat("Run",reportItem.Name));
+            return report.ResultsCount() != 0 || force
+                    ? new Export.HtmlExport(report, reportItem).SaveFile(prefix, "html")
+                    : null;
+        }   
+    }
+}
---
+++ Branches/EmailReportUpdates/ASR/Controls/DateTimePicker.cs
@@ -0,0 +1,42 @@
+﻿using Sitecore;
+
+namespace ASR.Controls
+{
+    public class DateTimePicker : Sitecore.Web.UI.HtmlControls.DateTimePicker
+    {
+        public string Format
+        {
+            get { return base.GetViewStateString("Value.Format"); }
+            set
+            {
+                base.SetViewStateString("Value.Format", value);
+            }
+        }
+
+        public DateTimePicker():base()
+        {
+            Format = "yyyyMMddTHHmmss";
+        }
+
+        protected override void DoRender(System.Web.UI.HtmlTextWriter output)
+        {
+            base.DoRender(output);
+        }
+        public override string Value
+        {
+            get
+            {
+                var value = base.Value;
+                if(DateUtil.IsIsoDate(value))
+                {                    
+                    return DateUtil.IsoDateToDateTime(value).ToString(Format);
+                }
+                return value;
+            }
+            set
+            {                
+               base.Value = Util.MakeDateReplacements(value);
+            }
+        }
+    }
+}
